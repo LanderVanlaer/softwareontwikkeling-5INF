9b406b5ecfe92bde568fd2ddcc87478a8a96bef3
Lander Van laer <lander.vanlaer1@gmail.com>
2020-05-05 12:04:59 +0200

    txt file fix

--- src/basic/trim2/nDarrays/oef9_1_1.java
+++ src/basic/trim2/nDarrays/oef9_1_1.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class oef9_1_1 {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("oef9_1_1.txt"));
+        Scanner scan = new Scanner(new File("src/basic/trim2/nDarrays/oef9_1_1.txt"));
         String[][] data = new String[4][3];
         for(int i = 0; i < data.length; i++) {
             for(int j = 0; j < data[i].length; j++) {
