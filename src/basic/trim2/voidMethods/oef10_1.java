a1407985c96bc1b9d96804d433cb6f7b45bf4cf5
Lander Van laer <lander.vanlaer1@gmail.com>
2020-05-05 11:23:55 +0200

    voidMethods

---
+++ src/basic/trim2/voidMethods/oef10_1.java
@@ -0,0 +1,27 @@
+
+public class oef10_1 {
+	public static void main(String[] args) {
+		drawW();
+		System.out.println();
+		drawO();
+		System.out.println();
+		drawW();
+	}
+
+	public static void drawW() {
+		System.out.println("**          **");
+		System.out.println(" **        **");
+		System.out.println("  **  **  **");
+		System.out.println("   **    **");
+	}
+
+	public static void drawO() {
+		System.out.println("     ****");
+		System.out.println("   **    **");
+		System.out.println(" **        **");
+		System.out.println("**          **");
+		System.out.println(" **        **");
+		System.out.println("   **    **");
+		System.out.println("     ****");
+	}
+}
